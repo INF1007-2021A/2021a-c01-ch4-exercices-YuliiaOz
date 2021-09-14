2b71075c10fbc75458157f2c8ad27d6c3962df58
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2021-09-14 17:18:23 -0400

    fini

--- exercice.py
+++ exercice.py
@@ -3,22 +3,61 @@
 
 
 def is_even_len(string: str) -> bool:
+    longueur = (len(string))
+    #print(len(string))
+    reste = longueur%2
+    paire_impaire = (reste == 0)
+    return paire_impaire
     pass
 
 
 def remove_third_char(string: str) -> str:
+    longueur = (len(string))
+    #print('hello world' [2:3])
+    nouveau_mot = (string[:2]+ string[3:])
+    return nouveau_mot
     pass
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
+    caractere = list(string)
+    #print(caractere)
+    caractere[6] = 'z'
+    new_char = "".join(caractere)
+    #print(type(new_char))
+    return new_char
     pass
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    pass
+    #longueur = len(string)
+    a = 0
+    nombre_occurence = 0
+
+    while a < len(string):
+        if string[a] == 'l':
+            nombre_occurence += 1
+        a +=1
+
+    nombre_occurence-=1
+
+    return nombre_occurence
+    pass #ne pas utiliser la fonction count
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
+    liste_mots = sentence.split()
+    print(liste_mots)
+
+    a = 0
+    nombre_occurence = 0
+
+    while a < len(liste_mots):
+        if liste_mots[a] == 'doo':
+            nombre_occurence += 1
+        a += 1
+
+    return nombre_occurence
     pass
 
 
